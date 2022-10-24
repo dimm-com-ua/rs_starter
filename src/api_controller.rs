8b6652e78cd5da6a98d5518c7d6f530b78aecd1d
dimm-com-ua <59435056+dimm-com-ua@users.noreply.github.com>
2022-10-24 20:53:42 +0300

    Dockerfile

--- src/api_controller.rs
+++ src/api_controller.rs
@@ -7,11 +7,17 @@ const API_ROOT: &str = "/api/v1";
 #[inline]
 pub fn routes(rocket_build: Rocket<Build>) -> Rocket<Build> {
     rocket_build.mount(API_ROOT.to_string(), routes![
-        hello
+        hello,
+        health
     ])
 }
 
 #[get("/hello")]
 fn hello() -> ApiResponse {
     ApiResponse::app_api_success(Some(json!({"hello": "World!"})))
+}
+
+#[get("/health")]
+fn health() -> String {
+    "I'm ok".to_string()
 }
